2b771fc3b9dd72fe921a7b28658a49e88cc89701
tt2468 <tt2468@gmail.com>
2021-11-17 00:33:23 -0800

    Requests: Enable GetRecordDirectory

--- src/requesthandler/RequestHandler.cpp
+++ src/requesthandler/RequestHandler.cpp
@@ -96,7 +96,7 @@ const std::map<std::string, RequestMethodHandler> RequestHandler::_handlerMap
 	{"ToggleRecordPause", &RequestHandler::ToggleRecordPause},
 	{"PauseRecord", &RequestHandler::PauseRecord},
 	{"ResumeRecord", &RequestHandler::ResumeRecord},
-	//{"GetRecordDirectory", &RequestHandler::GetRecordDirectory},
+	{"GetRecordDirectory", &RequestHandler::GetRecordDirectory},
 };
 
 RequestHandler::RequestHandler(SessionPtr session) :
